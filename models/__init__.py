a480f7d649208be5d0885338c6c6928352bbdb63
= <=>
2023-07-14 00:42:47 +0300

    update on base model

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#! /usr/bin/python3
+"""init file"""
+
+
+classes = {'BaseModel': 'BaseModel'}
--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,22 @@
+#! /usr/bin/python3
+"""base class"""
+
 import datetime
 from uuid import uuid4
 
+
 class BaseModel:
-    def __init__ (self):
+    """actual base class"""
+
+    def __init__(self):
+        """instance of base class"""
         self.id = str(uuid4())
         self.created_at = datetime.datetime.now().isoformat()
         self.updated_at = datetime.datetime.now().isoformat()
 
     def __str__(self):
-        """magic method __str__ to print [<class name>] (<self.id>) <self.__dict__>	"""
+        """magic method __str__ to print [<class name>] (<self.id>)
+          <self.__dict__>	"""
         class_name = self.__class__.__name__
         return f"[{class_name}] ({self.id}) {self.__dict__}"
 
@@ -17,12 +25,9 @@ class BaseModel:
         self.updated_at = datetime.now
 
     def to_dict(self):
-        """function to return a dictionary containign all keys/values of __dict__
-        of the instance"""
+        """function to return a dictionary containign all
+        keys/values of __dict__ of the instance"""
         dictf = self.__dict__.copy()
         """identify the class name of the instance"""
-        dictf ["__class__"] = self.__class__.__name__
-
-        dictf["created_at"] = self.created_at
-
+        dictf["__class__"] = self.__class__.__name__
         return dictf
