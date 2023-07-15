7db2bc8d68295c3c7db02cf53e7f639d2e1a4518
= <=>
2023-07-15 16:52:27 +0300

    update on console

--- models/base_model.py
+++ models/base_model.py
@@ -1,30 +1,53 @@
 #! /usr/bin/python3
 """base class"""
-
 from datetime import datetime
 from uuid import uuid4
-from models import storage
+# import models
+import models
 
 
 class BaseModel:
     """actual base class"""
 
-    def __init__(self, *ars, **kwargs):
-        """instance of base class"""
-        if kwargs:
-            del kwargs["__class__"]
-            for key, val in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    new_val = val.isoformat()
-                    setattr(self, key, new_val)
-                else:
-                    setattr(self, key, val)
+    # def __init__(self, *args, **kwargs):
+    #     """instance of base class"""
+    #     if kwargs:
+    #         del kwargs["__class__"]
+    #         for key, val in kwargs.items():
+    #             if key == "created_at" or key == "updated_at":
+    #                 new_val = val.isoformat()
+    #                 setattr(self, key, new_val)
+    #             else:
+    #                 setattr(self, key, val)
 
+    #     else:
+    #         self.id = str(uuid4())
+    #         self.created_at = datetime.now()
+    #         self.updated_at = datetime.now()
+    #         storage.new(self)
+
+    def __init__(self, *args, **kwargs):
+        """ Construct """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
+                setattr(self, key, value)
         else:
             self.id = str(uuid4())
-            self.created_at = datetime.now().isoformat()
-            self.updated_at = datetime.now().isoformat()
-            storage.new(self)
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """magic method __str__ to print [<class name>] (<self.id>)
@@ -34,8 +57,8 @@ class BaseModel:
 
     def save(self):
         """Updated the public instance attribute with current time"""
-        self.updated_at = datetime.now
-        storage.save()
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """function to return a dictionary containign all
@@ -43,4 +66,6 @@ class BaseModel:
         dictf = self.__dict__.copy()
         """identify the class name of the instance"""
         dictf["__class__"] = self.__class__.__name__
+        dictf['created_at'] = self.created_at.isoformat()
+        dictf['updated_at'] = self.updated_at.isoformat()
         return dictf
