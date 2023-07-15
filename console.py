dbf9cd9073ab47c3724b5caff90bc503cfcecc1d
= <=>
2023-07-15 23:29:01 +0300

    count instances option

--- console.py
+++ console.py
@@ -129,6 +129,16 @@ class HBNBCommand(cmd.Cmd):
                     print('** attribute name missing **')
             else:
                 print('** no instance found **')
+                
+    def do_count(self, arg):
+        """Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class."""
+        argl = arg.split()
+        count = 0
+        for obj in storage.all().values():
+            if argl[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
 
 
 if __name__ == "__main__":
