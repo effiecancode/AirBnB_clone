9bddd91d3b23d7838554bc9cba3ef1fecc2b21fd
= <=>
2023-07-16 01:35:45 +0300

    redo of the console

--- console.py
+++ console.py
@@ -1,21 +1,32 @@
 #! /usr/bin/python3
 """console"""
 import cmd
+import shlex
 import sys
-import json
-import os
 from models import storage
 from models.base_model import BaseModel
-from models import amenity, city, place, review, state, user
+from models.user import User
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
 
 
-sys.path.append("/home/njuguna/Desktop/alx/SE foundations/AirBnB_clone/models")
+sys.path.append("/home/njuguna/Desktop/alx/SEfoundations/AirBnB_clone/models")
 
 
 class HBNBCommand(cmd.Cmd):
     """console class"""
     prompt = "(hbnb) "
-    options = {"BaseModel": BaseModel}
+    options = {"BaseModel": BaseModel,
+               "Amenity": Amenity,
+               "City": City,
+               "Place": Place,
+               "Review": Review,
+               "State": State,
+               "User": User
+               }
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -32,62 +43,67 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """creates a new instance of BaseModel"""
-        if args:
-            args_list = args.split()
-            if len(args_list) == 1:
-                if args in self.options.keys():
-                    new_obj = self.options[args]()
-                    new_obj.save()
-                    print(new_obj.id)
-                else:
-                    print("** class doesn't exist **")
-        else:
+        args = shlex.split(args)
+        if len(args) == 0:
             print("** class name missing **")
+            return
+        if args[0] in self.options:
+            implementation = self.options[args[0]]
+        else:
+            print("** class doesn't exist **")
+            return
+        print(implementation.id)
+        BaseModel.save()
+        
+                    
 
     def do_show(self, args):
         """Prints the string representation of an instance
         based on the class name and id"""
+        args = shlex.split(args)
         if len(args) == 0:
             print("** class name missing **")
             return
-        my_dict = storage.all()
-        if args:
-            args_list = args.split()
-            if args_list[0] not in self.options.keys():
-                print("** class doesn't exist **")
-            elif len(args_list) == 1:
-                print("** instance id missing **")
-            elif f"{args_list[0]}.{args_list[1]}" not in my_dict:
-                print("** no instance found **")
+        if args[0] in self.options:
+            if len(args) > 1:
+                arg_id = f"{args[0]}.{args[1]}"
+                if arg_id in storage.all():
+                    print(storage.all()[arg_id])
+                else:
+                    print("** no instance found **")
             else:
-                print(my_dict[f"{args_list[0]}.{args_list[1]}"])
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+            
 
     def do_destroy(self, args):
         """ Deletes an instance based on the class name and id"""
-        if args:
-            arg_list = args.split()
-            try:
-                obj = eval(arg_list[0])
-            except Exception:
-                print("** class doesn't exist **")
-            if len(arg_list) == 1:
-                print('** instance id missing **')
-            if len(arg_list) > 1:
-                key = arg_list[0] + '.' + arg_list[1]
-                if key in storage.all():
-                    storage.all().pop(key)
+        args = shlex.split(args)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] in self.options:
+            if len(args) > 1:
+                args_id = f"{args[0]}.{args[1]}"
+                if args_id in storage.all():
+                    storage.all().pop(args_id)
                     storage.save()
                 else:
-                    print('** no instance found **')
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
         else:
-            print("** class name missing **")
+            print("** class doesn't exist **")
+            
 
     def do_all(self, args):
         """ Prints all string representation of all
         instances based or not on the class name. """
+        args = shlex.split(args)
         if len(args) == 0:
             print([str(a) for a in storage.all().values()])
-        elif args not in self.options:
+        elif args[0] not in self.options:
             print("** class doesn't exist **")
         else:
             print([str(a) for b, a in storage.all().items() if args in b])
@@ -118,6 +134,16 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print('** no instance found **')
 
+    def do_count(self, arg):
+        """Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class."""
+        argl = arg.split()
+        count = 0
+        for obj in storage.all().values():
+            if argl[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
+        
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
