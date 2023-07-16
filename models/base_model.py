d9e134b56c28219500ca9512317731368da130f0
= <=>
2023-07-16 22:19:19 +0300

    bug fix 2

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,7 @@ class BaseModel:
     def save(self):
         """Updated the public instance attribute with current time"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at = dt.now()
         storage.save()
 
     def to_dict(self):
