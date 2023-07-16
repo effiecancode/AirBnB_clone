0ae19033169b4fdec586e3515dde64d62e4ec6d5
= <=>
2023-07-16 17:15:58 +0300

    added <class name>.all() functionality

--- console.py
+++ console.py
@@ -40,6 +40,25 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """empty line"""
         pass
+    
+    def __getattr__(self, attr):
+        """Custom attribute getter to enable ClassName.all() syntax"""
+        class_name = attr.split(".")[0]
+        if class_name in self.options:
+            return lambda: self.do_all(class_name)
+        else:
+            raise AttributeError(f"'HBNBCommand' object has no attribute '{attr}'")
+   
+    def default(self, line):
+        """Handles the ClassName.all() syntax"""
+        tokens = line.split('.')
+        if len(tokens) == 2 and tokens[1] == 'all()':
+            class_name = tokens[0]
+            if class_name in self.options:
+                self.do_all(class_name)
+                return
+        super().default(line)
+
 
     def do_create(self, arg):
         """ Create a new instance """
@@ -94,15 +113,30 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
+    # def do_all(self, args):
+    #     """ Prints all string representation of all
+    #     instances based or not on the class name. """
+    #     if len(args) == 0:
+    #         print([str(a) for a in storage.all().values()])
+    #     elif args not in self.options:
+    #         print("** class doesn't exist **")
+    #     else:
+    #         print([str(a) for b, a in storage.all().items() if args in b])
+    
     def do_all(self, args):
-        """ Prints all string representation of all
-        instances based or not on the class name. """
-        if len(args) == 0:
-            print([str(a) for a in storage.all().values()])
-        elif args not in self.options:
+        """Prints all string representation of all
+        instances based or not on the class name."""
+        if args and args in self.options:
+            class_name = args
+            instances = storage.all()
+            filtered_instances = [str(obj) for obj in instances.values() if isinstance(obj, self.options[class_name])]
+            print(filtered_instances)
+        elif args and args not in self.options:
             print("** class doesn't exist **")
         else:
-            print([str(a) for b, a in storage.all().items() if args in b])
+            print([str(obj) for obj in storage.all().values()])
+
+
 
     def do_update(self, arg):
         """ updates an instance"""
@@ -130,16 +164,22 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print('** no instance found **')
 
-    def do_count(self, arg):
-        """Usage: count <class> or <class>.count()
-        Retrieve the number of instances of a given class."""
-        argl = arg.split()
-        count = 0
-        for obj in storage.all().values():
-            if argl[0] == obj.__class__.__name__:
-                count += 1
-        print(count)
-
+    # def do_count(self, args):
+    #     """Usage: count <class> or <class>.count()
+    #     Retrieve the number of instances of a given class."""
+    #     # argl = shlex.split(arg)
+    #     # count = 0
+    #     # for obj in storage.all().values():
+    #     #     if argl[0] == obj.__class__.__name__:
+    #     #         count += 1
+    #     # print(count)
+    #     args = shlex.split(args)
+    #     my_dict = storage.all()
+    #     appearances = [
+    #         appearance for appearance in my_dict if 
+    #         appearance.startswith("." + args[0])
+    #     ]
+    #     print(len(appearances))
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
