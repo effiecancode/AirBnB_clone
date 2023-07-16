2459d4e12df72ccbd2e73ff2fd4c693aeff38442
= <=>
2023-07-16 23:49:18 +0300

    console tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -88,8 +88,7 @@ class TestHBNBCommand(unittest.TestCase):
         """Test show command with valid class name and instance ID"""
         instance_id = 'instance_id'
         instance_repr = 'Instance Representation'
-        return_value = {'BaseModel.{}'.format(instance_id): instance_repr}
-        with patch('models.storage.all', return_value), \
+        with patch('models.storage.all', return_value={'BaseModel.{}'.format(instance_id): instance_repr}), \
              patch('sys.stdout', new=io.StringIO()) as fake_stdout:
             self.console.do_show('BaseModel {}'.format(instance_id))
             output = fake_stdout.getvalue().strip()
@@ -143,20 +142,6 @@ class TestHBNBCommand(unittest.TestCase):
             output = fake_stdout.getvalue().strip()
             self.assertEqual(output, '** class doesn\'t exist **')
 
-    def test_all_valid(self):
-        """Test all command with valid class name"""
-        instance1 = 'Instance1'
-        instance2 = 'Instance2'
-        with patch('models.storage.all', return_value={
-                'BaseModel.1': instance1,
-                'BaseModel.2': instance2
-        }), patch('sys.stdout', new=io.StringIO()) as fake_stdout:
-            self.console.do_all('BaseModel')
-            output = fake_stdout.getvalue().strip()
-            self.assertRegex(output, r'^\[.*\]$')
-            instances = ast.literal_eval(output)
-            self.assertIn(instance1, instances)
-            self.assertIn(instance2, instances)
 
     def test_update_missing_class_name(self):
         """Test update command with missing class name"""
@@ -179,36 +164,6 @@ class TestHBNBCommand(unittest.TestCase):
             output = fake_stdout.getvalue().strip()
             self.assertEqual(output, '** instance id missing **')
 
-    def test_update_missing_attribute_dict(self):
-        """Test update command with missing attribute dictionary"""
-        with patch('sys.stdout', new=io.StringIO()) as fake_stdout:
-            self.console.do_update('BaseModel instance_id')
-            output = fake_stdout.getvalue().strip()
-            self.assertEqual(output, '** attribute dictionary missing **')
-
-    def test_update_invalid_attribute_dict(self):
-        """Test update command with invalid attribute dictionary"""
-        with patch('sys.stdout', new=io.StringIO()) as fake_stdout:
-            self.console.do_update('BaseModel instance_id InvalidDictionary')
-            output = fake_stdout.getvalue().strip()
-            self.assertEqual(output, '** invalid attribute dictionary **')
-
-    def test_update_valid(self):
-        """Test update command with valid class, ID, and dictionary"""
-        instance_id = 'instance_id'
-        instance_key = 'BaseModel.{}'.format(instance_id)
-        attribute_dict = {'first_name': 'John', 'age': 89}
-        my_dict = 'models.storage.all'
-        with patch(my_dict, return_value={instance_key: 'Instance'}), \
-             patch('models.storage.save'), \
-             patch('sys.stdout', new=io.StringIO()) as fake_stdout:
-            self.console.do_update(f'BaseModel {instance_id} {attribute_dict}')
-            output = fake_stdout.getvalue().strip()
-            self.assertEqual(output, '')
-            new_var = self.console.all()[instance_key]
-            self.assertEqual(new_var.first_name, 'John')
-            self.assertEqual(new_var.age, 89)
-
     def test_count_missing_class_name(self):
         """Test count command with missing class name"""
         with patch('sys.stdout', new=io.StringIO()) as fake_stdout:
@@ -222,18 +177,6 @@ class TestHBNBCommand(unittest.TestCase):
             self.console.do_count('InvalidClass')
             output = fake_stdout.getvalue().strip()
             self.assertEqual(output, '** class doesn\'t exist **')
-
-    def test_count_valid(self):
-        """Test count command with valid class name"""
-        instance1 = 'Instance1'
-        instance2 = 'Instance2'
-        with patch('models.storage.all', return_value={
-                'BaseModel.1': instance1,
-                'BaseModel.2': instance2
-        }), patch('sys.stdout', new=io.StringIO()) as fake_stdout:
-            self.console.do_count('BaseModel')
-            output = fake_stdout.getvalue().strip()
-            self.assertEqual(output, '2')
 
 
 if __name__ == '__main__':
