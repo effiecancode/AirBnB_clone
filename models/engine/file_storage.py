3010fc30e537313ce0689f14c2bab1aa841c8644
= <=>
2023-07-17 01:57:00 +0300

    tests

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,13 +5,6 @@
 -> <class 'BaseModel'>
 """
 import json
-# # from models.base_model import BaseModel
-# from models.user import User
-# from models.state import State
-# from models.city import City
-# from models.amenity import Amenity
-# from models.place import Place
-# from models.review import Review
 
 
 class FileStorage:
