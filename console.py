275a0e991430822d4d74bc591258149d7c87caea
= <=>
2023-07-16 18:12:51 +0300

    style update

--- console.py
+++ console.py
@@ -40,14 +40,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """empty line"""
         pass
-    
-    # def __getattr__(self, attr):
-    #     """Custom attribute getter to enable ClassName.function() syntax"""
-    #     class_name = attr.split(".")[0]
-    #     if class_name in self.options:
-    #         return lambda: self.do_all(class_name)
-    #     else:
-    #         raise AttributeError(f"'HBNBCommand' object has no attribute '{attr}'")
+
     def __getattr__(self, attr):
         """Custom attribute getter to enable ClassName.function() syntax"""
         class_name = attr.split(".")[0]
@@ -57,7 +50,7 @@ class HBNBCommand(cmd.Cmd):
             elif attr.endswith('.destroy'):
                 return lambda id: self.do_destroy(f"{class_name} {id}")
         raise AttributeError(f"'HBNBCommand' object has no attribute '{attr}'")
-    
+
     def default(self, line):
         """Handles the ClassName.all() syntax"""
         tokens = line.split('.')
@@ -67,7 +60,7 @@ class HBNBCommand(cmd.Cmd):
                 self.do_all(class_name)
                 return
         super().default(line)
-        
+
         """Handles the ClassName.count() syntax"""
         if len(tokens) == 2 and tokens[1] == 'count()':
             class_name = tokens[0]
@@ -75,25 +68,26 @@ class HBNBCommand(cmd.Cmd):
                 self.do_count(class_name)
                 return
         super().default(line)
-        
+
         """Handles the ClassName.show(id) syntax"""
-        if len(tokens) == 2 and tokens[1].startswith('show(') and tokens[1].endswith(')'):
-            class_name = tokens[0]
-            if class_name in self.options:
-                instance_id = tokens[1][5:-1]
-                self.do_show(f"{class_name} {instance_id}")
-                return
-        super().default(line)
-        
-        """Handles the ClassName.destroy(id) syntax"""
-        if len(tokens) == 2 and tokens[1].startswith('destroy(') and tokens[1].endswith(')'):
-            class_name = tokens[0]
-            if class_name in self.options:
-                instance_id = tokens[1][8:-1]
-                self.do_destroy(f"{class_name} {instance_id}")
-                return
+        if len(tokens) == 2 and tokens[1].startswith('show('):
+            if tokens[1].endswith(')'):
+                class_name = tokens[0]
+                if class_name in self.options:
+                    instance_id = tokens[1][5:-1]
+                    self.do_show(f"{class_name} {instance_id}")
+                    return
         super().default(line)
 
+        """Handles the ClassName.destroy(id) syntax"""
+        if len(tokens) == 2 and tokens[1].startswith('destroy('):
+            if tokens[1].endswith(')'):
+                class_name = tokens[0]
+                if class_name in self.options:
+                    instance_id = tokens[1][8:-1]
+                    self.do_destroy(f"{class_name} {instance_id}")
+                    return
+        super().default(line)
 
     def do_create(self, arg):
         """ Create a new instance """
@@ -110,25 +104,6 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     print("** class doesn't exist **")
 
-    # def do_show(self, args):
-    #     """Prints the string representation of an instance
-    #     based on the class name and id"""
-    #     args = shlex.split(args)
-    #     if len(args) == 0:
-    #         print("** class name missing **")
-    #         return
-    #     if args[0] in self.options:
-    #         if len(args) > 1:
-    #             arg_id = f"{args[0]}.{args[1]}"
-    #             if arg_id in storage.all():
-    #                 print(storage.all()[arg_id])
-    #             else:
-    #                 print("** no instance found **")
-    #         else:
-    #             print("** instance id missing **")
-    #     else:
-    #         print("** class doesn't exist **")
-    
     def do_show(self, args):
         """Prints the string representation of an instance
         based on the class name and id"""
@@ -151,25 +126,6 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
-
-    # def do_destroy(self, args):
-    #     """ Deletes an instance based on the class name and id"""
-    #     args = shlex.split(args)
-    #     if len(args) == 0:
-    #         print("** class name missing **")
-    #         return
-    #     if args[0] in self.options:
-    #         if len(args) > 1:
-    #             args_id = f"{args[0]}.{args[1]}"
-    #             if args_id in storage.all():
-    #                 storage.all().pop(args_id)
-    #                 storage.save()
-    #             else:
-    #                 print("** no instance found **")
-    #         else:
-    #             print("** instance id missing **")
-    #     else:
-    #         print("** class doesn't exist **")
     def do_destroy(self, args):
         """Deletes an instance based on the class name and id"""
         args = shlex.split(args)
@@ -192,31 +148,19 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
-
-    # def do_all(self, args):
-    #     """ Prints all string representation of all
-    #     instances based or not on the class name. """
-    #     if len(args) == 0:
-    #         print([str(a) for a in storage.all().values()])
-    #     elif args not in self.options:
-    #         print("** class doesn't exist **")
-    #     else:
-    #         print([str(a) for b, a in storage.all().items() if args in b])
-    
     def do_all(self, args):
         """Prints all string representation of all
         instances based or not on the class name."""
         if args and args in self.options:
             class_name = args
             instances = storage.all()
-            filtered_instances = [str(obj) for obj in instances.values() if isinstance(obj, self.options[class_name])]
+            filtered_instances = [str(obj) for obj in instances.values()
+                                  if isinstance(obj, self.options[class_name])]
             print(filtered_instances)
         elif args and args not in self.options:
             print("** class doesn't exist **")
         else:
             print([str(obj) for obj in storage.all().values()])
-
-
 
     def do_update(self, arg):
         """ updates an instance"""
@@ -250,7 +194,8 @@ class HBNBCommand(cmd.Cmd):
         if len(arg_list) == 1:
             class_name = arg_list[0]
             if class_name in self.options:
-                count = sum(1 for obj in storage.all().values() if isinstance(obj, self.options[class_name]))
+                count = sum(1 for obj in storage.all().values()
+                            if isinstance(obj, self.options[class_name]))
                 print(count)
             else:
                 print("** class doesn't exist **")
